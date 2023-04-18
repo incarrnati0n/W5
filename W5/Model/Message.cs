b9bf2e96e42a992e5fcaae1a8e646e31ff886ce6
incarrnati0n <horvathimi2001@gmail.com>
2023-04-18 08:13:07 +0200

    model added

---
+++ W5/Model/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W5.Model
+{
+    public class Message
+    {
+        public string messageContent { get; set; }
+
+        public DateTime messageTime { get; set; }
+
+
+    }
+}
