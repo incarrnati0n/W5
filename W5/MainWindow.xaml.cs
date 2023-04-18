f4ea4c06dd49c9df663bdc8345945d014a9962e8
incarrnati0n <horvathimi2001@gmail.com>
2023-04-18 08:40:07 +0200

    Send method started

--- W5/MainWindow.xaml.cs
+++ W5/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using W5.Model;
 
 namespace W5
 {
@@ -23,6 +24,11 @@ namespace W5
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Send_Click(object sender, RoutedEventArgs e)
+        {
+            var message = new Message()
         }
     }
 }
